93728e44c8721f94f2f51ef019123ff0c37ed764
Nolan Morache <nolan241@gmail.com>
2018-01-15 21:58:58 +0000

    Added search param to book model and linked fomr book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.search(params[:search])
   end
 
   # GET /books/1
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ActiveRecord::Base
+  def self.search(search)
+    if search
+        @books = Book.where(["name LIKE ?","%#{search}%"])
+    else
+        all
+    end
+  end    
 end
